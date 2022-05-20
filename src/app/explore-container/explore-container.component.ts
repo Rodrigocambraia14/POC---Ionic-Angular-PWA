d2e8c87dc0e206064ccbf82638ebdc2e1912710f
Rodrigo Gonçalves Cambraia Soares <rodrigocambraia.jr@gmail.com>
2022-05-19 22:58:13 -0300

    POC de acesso à galeria via PWA

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
-
+  imgUrl: any;
+  webviewPath: string;
+  base64: string;
   constructor() { }
 
   ngOnInit() {}
+
+  onFileChoose(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    const pattern = /image-*/;
+    const reader = new FileReader();
+
+    if (!file.type.match(pattern)) {
+      return;
+    }
+
+    reader.onload = () => {
+      this.base64 = reader.result.toString();
+      this.webviewPath = URL.createObjectURL(file);
+    };
+
+    reader.readAsDataURL(file);
+  }
 
 }
